4970f899a83abe9ad5e7eb22d32337a32dd6cb11
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-19 21:49:16 +0530

    [Akshay] added program to find out frquency of word

---
+++ Hash Tables/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Hash_Tables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash5 = hash.Get("5");
+            Console.WriteLine(hash5);
+            hash.GetFreq();
+            
+
+        }
+    }
+}
